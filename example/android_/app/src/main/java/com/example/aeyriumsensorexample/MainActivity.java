c0bcbbe78a5223180e22266c578ce5b4dbf0f343
sqcsabbey <csabbey@signalquest.com>
2025-06-06 09:31:48 -0400

    Added start/stop commands

--- example/android_/app/src/main/java/com/example/aeyriumsensorexample/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.aeyriumsensorexample;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
--- example/android_/app/src/main/kotlin/com/sq/sensorexample/aeyrium_sensor_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sq.sensorexample.aeyrium_sensor_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
